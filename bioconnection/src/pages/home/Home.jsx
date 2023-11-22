8cc9923ceccbc273f5422c1872e5983b92435a51
Gabriel Benjamim <91507840+GabrielBdSilva@users.noreply.github.com>
2023-11-22 11:59:23 -0300

    Add files via upload

--- bioconnection/src/pages/home/Home.jsx
+++ bioconnection/src/pages/home/Home.jsx
@@ -1,9 +1,42 @@
+import './Home.css'
+import Home_img from "../../assets/home.png";
+import localizacao from "../../assets/localizacao.png";
+import telefone from "../../assets/telefone.png";
+import gmail from "../../assets/gmail.png";
+
+
 export default function Home() {
   
 
   return (
     <>
-      <h1>Aqui é o a tela Home da BIOCONNECTION</h1>
+    <div id="home">
+      <div>
+        <h2 >BIOCONNECTION</h2>
+        <h3>CONECTANDO SUA SAÚDE E <br />
+        TRANSFORMANDO VIDAS <br />
+        COM INOVAÇÃO BIOTECNOLÓGICA.</h3>
+      </div>
+      <div>
+        <img src={Home_img} alt="icone de localização" />
+        <div id='icones'>
+          <div>
+          <img src={telefone} alt="icone de telefone" />
+          <p>Av. Lins de Vasconcelos, 1222</p>
+          </div>
+          
+          <div>
+        <img src={localizacao} alt="icone de localização" />
+        <p>(11)2334-5566</p>
+          </div>
+
+          <div>
+        <img src={gmail} alt="img da tela home" />
+        <p>bio@gmail.com</p>
+          </div>
+        </div>
+      </div>
+      </div>
     </>
   );
 }
--- bioconnection/src/pages/sobre/Sobre.jsx
+++ bioconnection/src/pages/sobre/Sobre.jsx
@@ -1,11 +1,37 @@
-import "./Sobre.css"
-export default function Sobre() {
-  
+Sobre.jsx
 
-    return (
-      <>
-        <h1>aqui é a tela falando do q é a Bioconnection</h1>
-      </>
-    );
-  }
-  
+import "./Sobre.css";
+import sobre_img from "../../assets/sobre.png";
+export default function Sobre() {
+  return (
+    <><div id="sobre">
+      <div>
+        <h3>O QUE É A</h3>
+        <h2>BIOCONNECTION?</h2>
+        <p>
+          Fundada em 13 de novembro de 2023, a BioConnection é uma inovadora
+          empresa hospitalar que não apenas salva, mas também conecta vidas.
+          Reconhecendo a agitada rotina tanto dos médicos quanto dos pacientes
+          nos hospitais, um grupo de estudantes do Centro Universitário de
+          Informática e Administração Paulista desenvolveu esse projeto
+          visionário, que evoluiu para uma startup com o propósito de aprimorar
+          a experiência de ambos os lados. 
+          <br></br>
+          <br></br>
+          A BioConnection busca trazer mais
+          eficiência às unidades hospitalares por meio da implementação da
+          inteligência artificial em tarefas cotidianas. Essa abordagem visa
+          reduzir a lotação e o caos no ambiente hospitalar, proporcionando uma
+          prestação de serviços mais ágil e eficaz. Ao incorporar a tecnologia
+          de forma estratégica, a empresa torna a interação no ambiente
+          hospitalar mais leve, fácil e rápida, alinhando-se com a evolução
+          tecnológica que visa facilitar e melhorar nossas vidas.
+        </p>
+      </div>
+      <div>
+      <img src={sobre_img} alt="imagem do sobre"/>
+      </div>
+      </div>
+    </>
+  );
+}
